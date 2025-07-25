86644fdd727990744ce7c35f366838ef437ce36c
Rakhyvel <josephmshimel@gmail.com>
2025-07-24 20:13:14 -0600

    bolden top macros

---
+++ piecemeal_app/templatetags/macros.py
@@ -0,0 +1,33 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def floatval(value):
+    try:
+        return float(value)
+    except (ValueError, TypeError):
+        return 0.0
+
+
+@register.filter
+def mul(value, arg):
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return 0.0
+
+
+@register.filter
+def macro_ratio(macro_grams, total_calories):
+    if total_calories == 0:
+        return 0
+    return (macro_grams * 4) / total_calories  # 4 cals/g for protein/carbs, 9 for fat
+
+
+@register.filter
+def fats_ratio(macro_grams, total_calories):
+    if total_calories == 0:
+        return 0
+    return (macro_grams * 9) / total_calories  # 4 cals/g for protein/carbs, 9 for fat
