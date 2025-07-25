2a7c08ec9167b61bf66a7e432a1b94ff21ac478a
Rakhyvel <josephmshimel@gmail.com>
2025-07-25 17:44:01 -0600

    make pills cleaner

--- piecemeal_app/templatetags/format_extras.py
+++ piecemeal_app/templatetags/format_extras.py
@@ -13,3 +13,13 @@ def smart_float(value):
     if abs(num - round(num)) < 0.01:
         return f"{int(round(num))}"
     return f"{num:.1f}"
+
+
+@register.filter
+def smart_int(value):
+    try:
+        num = float(value)
+    except (ValueError, TypeError):
+        return value
+
+    return f"{int(round(num))}"
